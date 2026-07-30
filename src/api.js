5436302f5f3b8a58a16eedc27fd0c36d641f7bfd
NemoBJJ <engnfaraujo@gmail.com>
2026-07-30 16:43:54 -0300

    Migra backend NFSe para VPS Contabo

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const isDevelopment =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
 
 const BASE_URL = isDevelopment
-  ? 'http://localhost:8085/api'          // Porta 8085 = NFSe
-  : 'https://nfse-api-hkf5.onrender.com/api';  // URL do Render
+  ? 'http://localhost:8085/api'
+  : 'https://nfse-container.neemindev.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
